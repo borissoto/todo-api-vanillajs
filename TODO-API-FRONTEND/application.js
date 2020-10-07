5de8ec928089918c1a8f08633e7fc31d5e4de72d
Borrison <borissoto@gmail.com>
2020-10-07 18:06:27 -0400

    adding cors and frontend

---
+++ TODO-API-FRONTEND/application.js
@@ -0,0 +1,65 @@
+var elementAdditionPosition = 'beforeend';
+
+function getTasks() {
+  //   alert('this ran!');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      //Add the json stuff
+      let container = document.getElementById('container');
+      let responses = JSON.parse(this.responseText);
+      //   console.log(this.responseText);
+      //   window.responseText = this.responseText;
+
+      for (var i = 0; i < responses.length; i++) {
+        container.insertAdjacentHTML(
+          elementAdditionPosition,
+          createResponse(responses[i])
+        );
+      }
+    }
+  };
+  xhttp.open('GET', 'http://localhost:3001/todos', true);
+  xhttp.send();
+}
+
+window.sendForm = function (event) {
+  event.preventDefault();
+  var xhtto = new XMLHttpRequest();
+  xhtto.open('POST', 'http://localhost:3001/todos', true);
+  xhtto.onload = function (event) {
+    let container = document.getElementById('container');
+    let response = JSON.parse(event.target.response);
+    container.insertAdjacentHTML(
+      elementAdditionPosition,
+      createResponse(response)
+    );
+  };
+  var formData = new this.FormData(document.getElementById('todo_form'));
+  xhtto.send(formData);
+  document.getElementById('task_input').value = '';
+};
+
+function createResponse(response) {
+  return (
+    `<div class="task" id="${response.id}" onclick="deleteResponse('${response.id}')">` +
+    '[' +
+    response.id +
+    ']: ' +
+    response.task +
+    '<br />' +
+    '</div>'
+  );
+}
+
+function deleteResponse(response_id) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.open('DELETE', `http://localhost:3001/todos/${response_id}`, true);
+  xhttp.onload = function () {
+    let target = document.getElementById(response_id);
+    target.parentNode.removeChild(target);
+  };
+  xhttp.send(null);
+}
+
+getTasks();
